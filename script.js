9a26848bd5d143b39551945f1fee6ccbeca3f1dd
musharaffhussain <musharaffhussain.vjit@gmail.com>
2026-07-05 14:30:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 const viewer = document.getElementById("burgerViewer");
 
-document.getElementById("arBtn")
-.addEventListener("click", () => {
-  viewer.activateAR();
-});
+document
+  .getElementById("arBtn")
+  .addEventListener("click", () => {
+    viewer.activateAR();
+  });
 
-document.getElementById("pauseBtn")
-.addEventListener("click", () => {
-  viewer.autoRotate = !viewer.autoRotate;
-});
+document
+  .getElementById("pauseBtn")
+  .addEventListener("click", () => {
+    viewer.autoRotate = !viewer.autoRotate;
+  });
 
-document.getElementById("resetBtn")
-.addEventListener("click", () => {
-  viewer.cameraOrbit = "0deg 75deg 100%";
-});
+document
+  .getElementById("resetBtn")
+  .addEventListener("click", () => {
+    viewer.cameraOrbit = "0deg 75deg 100%";
+  });
